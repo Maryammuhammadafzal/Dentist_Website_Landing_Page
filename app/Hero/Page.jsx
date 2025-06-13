ebb75400767d2da86f11eba4daed722c5f973fe1
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-06-13 11:55:48 +0500

    creating Hero Section

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -10,9 +10,9 @@ const HeroPage = () => {
                 <Header/>
         </div>
       <div className="flex md:flex-row justify-end items-center h-full w-full">
-        <div className="h-full w-[45%] flex justify-start items-end">
+        <div className="h-full lg:w-[45%] w-[48%] flex justify-start items-end">
           <div className="flex flex-col gap-6 py-5 h-auto">
-            <h1 className="flex flex-col uppercase font-bold font-mono leading-24 text-[90px]">
+            <h1 className="flex flex-col uppercase font-bold font-mono lg:leading-20 2xl:leading-24 lg:text-7xl md:text-6xl text- 2xl:text-[90px]">
               <span className="text-primary">Your smile,</span>
               <span className="text-black">Our Passion</span>
             </h1>
@@ -26,12 +26,14 @@ const HeroPage = () => {
             </div>
           </div>
         </div>
-        <div className="w-[50%] h-full flex justify-end items-start">
+        <div className="w-[50%] max-md:w-full max-md:absolute max-md:left-0 max-md:top-0 max-md:-z-10 h-full flex justify-end items-start">
+                <div className="overlay max-md:block hidden w-full h-full bg-black absolute top-0 left-0 opacity-20 z-10"></div>
+                
                 <Image 
                 src={HeroImage.src}
                 width={300}
                 height={400}
-                className="w-full h-full rounded-bl-[20%] object-bottom-right object-cover" />
+                className="w-full h-full rounded-bl-[20%] object-center lg:object-bottom-right object-cover" />
         </div>
       </div>
     </div>
--- components/Header.jsx
+++ components/Header.jsx
@@ -25,15 +25,15 @@ const Header = () => {
   ];
   return (
     <header className="flex w-full font-mono h-full justify-center items-center py-5 px-3">
-      <div className="flex justify-between items-center h-auto lg:w-[85%] w-full">
+      <div className="flex justify-between items-center h-auto xl:w-[85%] lg:w-[95%] w-full">
         <a href="/" className="logo">
           <img src={Logo.src} alt="logo" className="w-auto h-auto" />
         </a>
-        <nav className="w-auto h-auto flex justify-center items-center">
-          <ul className="w-auto flex gap-3 justify-center items-center">
+        <nav className="w-auto h-auto flex justify-center items-center ">
+          <ul className="w-auto md:flex gap-1 justify-center items-center hidden">
            {nav_data.map(({link , name},index)=> (
-                 <li className="p-2 flex justify-center items-center">
-              <a href={link} className="text-black hover:text-primary font-medium active:text-primary focus:text-primary text-lg">
+                 <li className="lg:p-2 p-[5px] flex justify-center items-center">
+              <a href={link} className="text-black hover:text-primary font-medium active:text-primary focus:text-primary text-sm lg:text-lg">
                 {name}
               </a>
             </li>
